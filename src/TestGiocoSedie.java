957c91136bd332c73f8e89f2ddd2796e9c0090bd
Gab2006 <tostigabriele6@gmail.com>
2025-01-14 13:06:59 +0100

    secondo commit

--- src/TestGiocoSedie.java
+++ src/TestGiocoSedie.java
@@ -1,7 +1,7 @@
 
 
 import java.util.logging.Logger;
-
+import java.io.*;
 /**
  *
  * @author MC
@@ -13,9 +13,10 @@ public class TestGiocoSedie {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Posto sedie[] = new Posto[NUMSEDIE];
-
+        PrintStream ps = new PrintStream(new File("Risultato.txt"));
+        System.setOut(ps);
 	for (int k = 0; k < sedie.length; k++)
 		sedie[k] = new Posto();
 
